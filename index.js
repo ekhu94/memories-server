50e74eb006ab50218e61542a18a4eebfaac33ea9
Erik Huang <ekhu94@gmail.com>
2021-04-29 09:39:22 -0700

    successfully connects to MongoDB, imports .env connection url and port

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+console.log(process.env.CONNECTION_URL);
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connection to Mongo Open"))
+  .catch((err) => console.log(err.message));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on Port ${PORT}`);
+});
